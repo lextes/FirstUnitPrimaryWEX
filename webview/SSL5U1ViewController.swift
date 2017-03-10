406745d0435bff01a86869c5a0bf18eeae6f7eda
alex <arodriguez1981@gmail.com>
2017-03-10 07:57:07 -0600

    - Creando Slider para imágenes en Unit 1 lesson 5 Social Studies - Modificando comportamiento de videos y botones en Unit 1 Lesson 3 Social Studies

---
+++ webview/SSL5U1ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  SSL5U1ViewController.swift
+//  First Unit
+//
+//  Created by Alexander Rodriguez Torres on 09/03/17.
+//  Copyright © 2017 Desarrollo App Wexford. All rights reserved.
+//
+
+import UIKit
+import ImageSlideshow
+
+class SSL5U1ViewController: UIViewController {
+
+    @IBOutlet weak var imgFondo: UIImageView!
+    @IBOutlet weak var slideImagnes: ImageSlideshow!
+    let localSource = [ImageSource(imageString: "Image1")!, ImageSource(imageString: "Image2")!, ImageSource(imageString: "Image3")!, ImageSource(imageString: "Image4")!, ImageSource(imageString: "Image5")!, ImageSource(imageString: "Image6")!]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        slideImagnes.isHidden = true
+        
+        let recognizerFondo = UITapGestureRecognizer(target: self, action: #selector(SSL5U1ViewController.didTapFondo))
+        imgFondo.addGestureRecognizer(recognizerFondo)
+
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    @IBAction func showSlide(_ sender: Any) {
+        slideImagnes.isHidden = false
+        slideImagnes.setImageInputs(localSource)
+        
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(SSL5U1ViewController.didTap))
+        slideImagnes.addGestureRecognizer(recognizer)
+        
+        
+        slideImagnes.backgroundColor = UIColor.white
+        slideImagnes.slideshowInterval = 5.0
+        slideImagnes.pageControlPosition = PageControlPosition.underScrollView
+        slideImagnes.pageControl.currentPageIndicatorTintColor = UIColor.lightGray
+        slideImagnes.pageControl.pageIndicatorTintColor = UIColor.black
+        slideImagnes.contentScaleMode = UIViewContentMode.scaleAspectFit
+        slideImagnes.currentPageChanged = { page in
+            print("current page:", page)
+        }
+        
+    }
+    
+    func didTap() {
+        slideImagnes.presentFullScreenController(from: self)
+    }
+    
+    func didTapFondo() {
+        slideImagnes.isHidden = true
+    }
+
+
+}
--- webview/VideoViewController.swift
+++ webview/VideoViewController.swift
@@ -6,19 +6,26 @@
 //  Copyright © 2017 Desarrollo App Wexford. All rights reserved.
 //
 
+//  Modified by Alexander Rodriguez Torres on 09/03/17.
+//  Copyright © 2017 Desarrollo App Wexford. All rights reserved.
+
 import UIKit
 import AVFoundation
 import AVKit
 
 class VideoViewController: UIViewController {
     
+    @IBOutlet weak var viewButtons: UIView!
     var playerController = AVPlayerViewController()
     var player:AVPlayer?
     
+    var reproducido = false
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        self.viewButtons.isHidden = true
         let videoString: String? = Bundle.main.path(forResource: "trans", ofType: ".mp4")
         if let url = videoString{
             let videoURL = NSURL(fileURLWithPath: url)
@@ -33,12 +40,35 @@ class VideoViewController: UIViewController {
     }
     
     @IBAction func playVideo(_ sender: Any) {
-        self.present(self.playerController, animated: true, completion: {
-            
-            self.playerController.player?.play()
-        })
+        if (reproducido == false){
+            self.present(self.playerController, animated: true, completion: {
+                self.playerController.player?.play()
+                self.reproducido = true
+                self.viewButtons.isHidden = false
+            })
+        }
+        
     }
     
-    
+    @IBAction func seleccionar1(_ sender: Any) {
+        self.viewButtons.isHidden = true
+        reproducido = false
+    }
+    @IBAction func seleccionar2(_ sender: Any) {
+        self.viewButtons.isHidden = true
+        reproducido = false
+    }
+    @IBAction func seleccionar3(_ sender: Any) {
+        self.viewButtons.isHidden = true
+        reproducido = false
+    }
+    @IBAction func seleccionar4(_ sender: Any) {
+        self.viewButtons.isHidden = true
+        reproducido = false
+    }
+    @IBAction func seleccionar5(_ sender: Any) {
+        self.viewButtons.isHidden = true
+        reproducido = false
+    }
 }
 
